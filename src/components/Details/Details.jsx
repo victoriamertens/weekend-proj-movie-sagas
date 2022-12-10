3decd5ae3d0068a168c60d893d4ef9f838b0e6c4
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-10 12:26:22 -0600

    Movie Details requested from server and set to store

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,4 +1,16 @@
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 function Details() {
-  return <p>Testing details page</p>;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getDetails();
+  }, []);
+  const getDetails = () => {
+    dispatch({ type: 'GET_MOVIE_DETAILS', payload: id });
+  };
+
+  return <p>Testing details page, {id}</p>;
 }
 export default Details;
--- src/index.js
+++ src/index.js
@@ -13,63 +13,81 @@ import axios from 'axios';
 
 // Create the rootSaga generator function
 function* rootSaga() {
-    yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+  yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+  yield takeEvery('GET_MOVIE_DETAILS', getMovieDetails);
 }
 
 function* fetchAllMovies() {
-    // get all movies from the DB
-    try {
-        const movies = yield axios.get('/api/movie');
-        console.log('get all:', movies.data);
-        yield put({ type: 'SET_MOVIES', payload: movies.data });
-
-    } catch {
-        console.log('get all error');
-    }
-        
+  // get all movies from the DB
+  try {
+    const movies = yield axios.get('/api/movie');
+    console.log('get all:', movies.data);
+    yield put({ type: 'SET_MOVIES', payload: movies.data });
+  } catch {
+    console.log('get all error');
+  }
 }
 
+function* getMovieDetails(action) {
+  try {
+    console.log(action.payload);
+    const response = yield axios.get(`/api/movie/${action.payload}`);
+    yield put({ type: 'SET_DETAILS', payload: response });
+    //need to get the genres too
+  } catch (error) {
+    alert(error);
+  }
+}
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
 // Used to store movies returned from the server
 const movies = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_MOVIES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_MOVIES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Used to store the movie genres
 const genres = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_GENRES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_GENRES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
+const details = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_DETAILS':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 // Create one store that all components can use
 const storeInstance = createStore(
-    combineReducers({
-        movies,
-        genres,
-    }),
-    // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+  combineReducers({
+    movies,
+    genres,
+    details,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={storeInstance}>
-        <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
