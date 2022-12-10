02ab89d33fa3651eb203279589768932a7d643ad
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-10 12:02:40 -0600

    Details page rendering

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,19 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
+import MovieList from '../MovieList/MovieList';
+import Details from '../Details/Details.jsx';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+        <Route exact path="/details/:id">
+          <Details />
+        </Route>
         {/* Details page */}
 
         {/* Add Movie page */}
@@ -18,6 +21,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,4 @@
+function Details() {
+  return <p>Testing details page</p>;
+}
+export default Details;
